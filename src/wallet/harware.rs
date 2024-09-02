3ceec003355884e595e53a521e683eeaa849285e
nzengi <zenginureddin1@gmail.com>
2024-09-02 14:21:56 +0300

    Update harware.rs

--- src/wallet/harware.rs
+++ src/wallet/harware.rs
@@ -1,22 +1,15 @@
 // src/wallet/hardware.rs
 
-use std::error::Error;
-
 pub struct HardwareWallet;
 
 impl HardwareWallet {
-    pub fn connect_ledger() -> Result<(), Box<dyn Error>> {
-        // Ledger cüzdan bağlantı kodu burada
+    pub fn connect_ledger() -> Result<(), String> {
+        // Ledger donanım cüzdanı ile bağlantı kurma kodu
         Ok(())
     }
 
-    pub fn connect_trezor() -> Result<(), Box<dyn Error>> {
-        // Trezor cüzdan bağlantı kodu burada
+    pub fn connect_trezor() -> Result<(), String> {
+        // Trezor donanım cüzdanı ile bağlantı kurma kodu
         Ok(())
-    }
-
-    pub fn sign_transaction_with_hardware_wallet() -> Result<String, Box<dyn Error>> {
-        // Donanım cüzdan ile işlem imzalama kodu burada
-        Ok("signed_transaction".to_string())
     }
 }
