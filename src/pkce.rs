b3da33d92c17ab67438c762f0b26d81ac5954b68
nzengi <zenginureddin1@gmail.com>
2024-09-02 14:22:16 +0300

    Update pkce.rs

--- src/pkce.rs
+++ src/pkce.rs
@@ -3,7 +3,6 @@
 use base64::{encode_config, URL_SAFE_NO_PAD};
 use sha2::{Digest, Sha256};
 use rand::Rng;
-use std::error::Error;
 
 pub struct PkceCode {
     pub code_verifier: String,
@@ -11,14 +10,14 @@ pub struct PkceCode {
 }
 
 impl PkceCode {
-    pub fn new() -> Result<Self, Box<dyn Error>> {
+    pub fn new() -> Self {
         let code_verifier = Self::generate_code_verifier();
-        let code_challenge = Self::generate_code_challenge(&code_verifier)?;
+        let code_challenge = Self::generate_code_challenge(&code_verifier);
 
-        Ok(PkceCode {
+        PkceCode {
             code_verifier,
             code_challenge,
-        })
+        }
     }
 
     fn generate_code_verifier() -> String {
@@ -26,8 +25,8 @@ impl PkceCode {
         encode_config(&verifier_bytes, URL_SAFE_NO_PAD)
     }
 
-    fn generate_code_challenge(code_verifier: &str) -> Result<String, Box<dyn Error>> {
+    fn generate_code_challenge(code_verifier: &str) -> String {
         let hash = Sha256::digest(code_verifier.as_bytes());
-        Ok(encode_config(&hash, URL_SAFE_NO_PAD))
+        encode_config(&hash, URL_SAFE_NO_PAD)
     }
 }
